b0310a7763cd33dd0fd88b7fa3d259092090dce1
Mehrdad <mohajer.mhr@gmail.com>
2019-12-23 10:52:36 +0330

    Add files via upload
    Additional files to project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ProcessBlacklist
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
